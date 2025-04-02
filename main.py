9a4b22dc6250fdb86fcd499f96dcd1f2d89c3b1f
zen <atthezenith@proton.me>
2025-04-02 23:36:17 +0300

    Release v1.1.0
    + File restructure
    + Hover to show element
    + Interface Styling
    + New Icon

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import json
 import os
-from PySide6.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QPushButton
+from PySide6.QtGui import QIcon
 from PySide6.QtWebEngineWidgets import QWebEngineView
+from PySide6.QtWidgets import QApplication, QFrame, QHBoxLayout, QLineEdit, QPushButton, QVBoxLayout, QWidget
 from rdkit import Chem
 from rdkit.Chem import rdDistGeom
 import requests
 import sys
 
-def get_file(file):
+def get_file(file: str):
     return str.format("file:///{dir}", dir = os.path.abspath(file).replace("\\", "/"))
 
 def get_struct(compound):
@@ -18,7 +19,7 @@ def get_struct(compound):
         os.makedirs("./cache")
     if not os.path.exists("./cache/3Dmol-min.js"):
         with open("./cache/3Dmol-min.js", "w") as f:
-            f.write(requests.get("https://3Dmol.org/build/3Dmol-min.js").text)
+            f.write(requests.get("https://cdnjs.cloudflare.com/ajax/libs/3Dmol/2.4.1/3Dmol-min.js").text)
     if os.path.exists(f"./cache/{compound.lower()}.pdb"):
         with open(f"./cache/{compound.lower()}.pdb", "r") as f:
             return f.read()
@@ -75,25 +76,33 @@ class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Compound SMILES Search")
-        self.setGeometry(200, 200, 800, 600)
+        self.setWindowTitle("pyqtmol")
+        self.setWindowIcon(QIcon("./assets/64x.ico"))
+        self.showNormal()
         
-        layout = QVBoxLayout()
-        
+        main_layout = QVBoxLayout()
+        top_layout = QHBoxLayout()
+        wv_layout = QVBoxLayout()
+
         self.compound_input = QLineEdit(self)
         self.compound_input.setPlaceholderText("Enter compound name")
-        layout.addWidget(self.compound_input)
-        
+        top_layout.addWidget(self.compound_input)
         self.search_button = QPushButton("Search", self)
-        layout.addWidget(self.search_button)
-
-        self.web_view = QWebEngineView(self)
-        self.web_view.setUrl(get_file('none.html'))
-        layout.addWidget(self.web_view)
+        top_layout.addWidget(self.search_button)
+        main_layout.addLayout(top_layout)
+        
+        webview_holder = QFrame(self)
+        self.webview_frame = QWebEngineView(self)
+        self.webview_frame.setUrl(get_file('./assets/none.html'))
+        wv_layout.addWidget(self.webview_frame)
+        webview_holder.setLayout(wv_layout)
+        main_layout.addWidget(webview_holder)
         
         self.search_button.clicked.connect(self.search_compound)
 
-        self.setLayout(layout)
+        self.setLayout(main_layout)
+        with open("./assets/styles.qss", "r") as style:
+            self.setStyleSheet(style.read())
     
     def search_compound(self):
         compound_name = self.compound_input.text().strip()
@@ -104,18 +113,17 @@ class MainWindow(QWidget):
     def display_3d_structure(self, struct):
         struct_str = '\\n'.join(struct.splitlines())
         
-        with open('./template.html', 'r') as fetch:
-            template = fetch.read()
-            template = template.replace('temp', struct_str)
-            file = open('./web.html', 'w')
-            file.write(template)
+        with open('./assets/template.html', 'r') as fetch:
+            data = fetch.read()
+            data = data.replace('temp', struct_str)
+            file = open('./cache/web.html', 'w')
+            file.write(data)
             file.close()
             fetch.close()
 
-        self.web_view.setUrl(get_file('web.html'))
+        self.webview_frame.setUrl(get_file('./cache/web.html'))
 
 
 app = QApplication(sys.argv)
 window = MainWindow()
-window.showFullScreen()
 sys.exit(app.exec())
